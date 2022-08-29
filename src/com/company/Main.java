5563b34c380a4d6316ba6b449ca4a093f8faa9ce
hooda.armaan <armaan.hooda040406@gmail.com>
2022-08-29 14:59:56 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        boolean A = false;
+        boolean B = false;
+        if (A == B)
+            System.out.println(true);
+        else
+            System.out.println(false);
+    }
+}
